928fea24501242d3e719e429cf7abaeabd89f5a6
Osiyomeoh <samuelaleonomoh5@gmail.com>
2024-02-09 11:13:05 +0100

    loginfix5

--- models/Merchant.js
+++ models/Merchant.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const dbConnect = require('../dbConnect');
 
 const merchantSchema = new mongoose.Schema({
     name: { type: String, required: true },
